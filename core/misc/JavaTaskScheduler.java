3478d0a32efbb9ee2d6aa301734d68dd54b2690f
LtMai <ltmai@users.noreply.github.com>
2021-09-10 21:24:25 +0200

    Refactor JavaTaskScheduler

--- core/misc/JavaTaskScheduler.java
+++ core/misc/JavaTaskScheduler.java
@@ -4,26 +4,23 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class JavaTaskScheduler {
+    private static final int INITIAL_DELAY = 2000;
+    private static final int EXECUTION_DELAY = 1000;
+    private static final int TASK_DURATION = 2000;
+    private static final int TASK_REPEATS = 5;
+
 
     public static void main(String[] args) {
 
         ScheduledExecutorService ses = Executors.newScheduledThreadPool(1);
 
         ses.scheduleWithFixedDelay(()->{
-           System.out.println(new Date()); 
-           try {
-            Thread.sleep(2000);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            System.out.println("----"); 
-        }, 5, 1, TimeUnit.MILLISECONDS);
+            System.out.println(new Date() + " Task started"); 
+            try { TimeUnit.MILLISECONDS.sleep(TASK_DURATION); } catch (Exception e) {}
+            System.out.println(new Date() + " Task completed"); 
+        }, INITIAL_DELAY, EXECUTION_DELAY, TimeUnit.MILLISECONDS);
 
-        try {
-            Thread.sleep(20000);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        try { TimeUnit.MILLISECONDS.sleep((TASK_DURATION+EXECUTION_DELAY)*TASK_REPEATS); } catch (Exception e) {}
 
         ses.shutdown();
     }
