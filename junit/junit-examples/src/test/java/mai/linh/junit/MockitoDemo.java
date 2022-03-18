2706cf45f82a0f71a6b9450ad311c1034f1cefbb
Linh-Tuan <linh.tuan.mai@enisco.com>
2022-03-18 20:20:12 +0100

    Verify spied object

--- junit/junit-examples/src/test/java/mai/linh/junit/MockitoDemo.java
+++ junit/junit-examples/src/test/java/mai/linh/junit/MockitoDemo.java
@@ -1,31 +1,56 @@
 package mai.linh.junit;
 
 import static org.assertj.core.api.BDDAssertions.then;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 /**
  * MockitoDemo
  */
 @ExtendWith(MockitoExtension.class)
+@ExtendWith(TestNameExtension.class)
 public class MockitoDemo {
 
+    @Captor
+    private ArgumentCaptor<String> captor;
 
+    @Spy
+    private List<String> spiedList = new ArrayList<>();
+    
     @Mock
     private List<String> mockedList;
+
+    @Test
+    void spiedListCanBeVerified()
+    {
+        // given
+        final String capturedText = "Carpe diem";
+        // when
+        spiedList.add(capturedText);
+        // then
+        verify(spiedList).add(captor.capture());
+        assertTrue(capturedText.equals(captor.getValue()));
+        assertFalse(spiedList.isEmpty());
+    }
     
     /**
      * Calulator#devide should throw ArithmeticException when deviding by zero
